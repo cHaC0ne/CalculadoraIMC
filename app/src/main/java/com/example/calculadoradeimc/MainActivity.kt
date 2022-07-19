b80932db06d6c72b348893f3d4df5740d177a432
Alumno <asd@asd.com>
2022-07-19 20:12:07 +0200

    faltan los snackbars

--- app/src/main/java/com/example/calculadoradeimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoradeimc/MainActivity.kt
@@ -3,20 +3,28 @@ package com.example.calculadoradeimc
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.SeekBar
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.example.calculadoradeimc.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var b: ActivityMainBinding
+    private var altura = 150
+    private var peso = 75
+    private var dobleAltura = 2.25
+    private var  IMC = 33.33
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         b = ActivityMainBinding.inflate(layoutInflater)
         setContentView(b.root)
-        val altura = b.sbAltura.progress.toString().plus("/200")
-        val peso =
+        customDialog()
+        b.imageView.setOnClickListener{customDialog()}
 
         b.sbAltura.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
-                b.tvSBAltura.text = seek.progress.toString().plus("/200")
+
+                b.tvSBAltura.text = progress.toString().plus("/200")
+                altura = progress
 
 
             }
@@ -27,12 +35,16 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onStopTrackingTouch(p0: SeekBar?) {
+                calcularIMC()
 
             }
         })
         b.sbPeso.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
-                b.tvSBPeso.text = seek.progress.toString().plus("/150")
+
+                b.tvSBPeso.text = progress.toString().plus("/150")
+                peso = progress
+
 
 
             }
@@ -43,12 +55,24 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onStopTrackingTouch(p0: SeekBar?) {
-
-            }
+                calcularIMC()
+                Toast.makeText(this@MainActivity, "$peso",Toast.LENGTH_SHORT).show()           }
         })
-        b.tvIMC.text ="$altura"
-
-
-
 }
+
+  fun calcularIMC() {
+      dobleAltura = altura.times(altura) / 10000.0
+      IMC = Math.round(peso / dobleAltura)
+          .times(100)
+          .div(100.0)
+      b.tvIMC.text = IMC.toString()
+  }
+    fun customDialog() {
+        val inflater = this.layoutInflater
+        val custom_layout = inflater.inflate(R.layout.alertdialog, null)
+        AlertDialog.Builder(this)
+            .setView(custom_layout)
+            .setPositiveButton("Aceptar", null)
+            .show()
+    }
 }
